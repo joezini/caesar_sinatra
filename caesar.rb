7a4e1abf6c4fe8ebbe6c773cdbe82cdba641f646
bigthinjoe <josephlukeedwards@gmail.com>
2016-07-31 18:52:22 +0100

    implement simple web interface for Caesar Cipher

---
+++ caesar.rb
@@ -0,0 +1,32 @@
+
+def caesar_cipher(input, shift)
+	output = ""
+	input.each_byte do |b|
+		output << cycle_char(b, shift).chr
+	end
+	output
+end
+
+def cycle_char(char_number, shift)
+	shifted = char_number + shift
+	if char_number >= 97 && char_number <= 122
+		# lower case
+		if shifted > 122
+			shifted = shifted - 123 + 97
+		end
+		return shifted
+	elsif char_number >= 65 && char_number <= 90
+		# upper case
+		if shifted > 90
+			shifted = shifted - 91 + 65
+		end
+		return shifted
+	else
+		# symbol
+		return char_number
+	end
+end
+
+#caesar_cipher("What a string!", 5)
+
+
---
+++ caesar_sinatra.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require_relative 'caesar'
+
+@@first_load = true
+
+get '/' do
+	message = params['message']
+	shift = params['shift']
+	if is_number(shift)
+		encoded_message = caesar_cipher(message, shift.to_i)
+	elsif @@first_load
+		encoded_message = "Please enter a message and the number of letters to shift by"
+	else
+		encoded_message = "Shift must be a number"
+	end
+	@@first_load = false
+	erb :index, :locals => {encoded_message: encoded_message}
+end
+
+def is_number(string)
+	!(/^[0-9]+$/.match(string).nil?)
+end
